e836597ea1a3e746d2041e4471cc6e149bdea998
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-04-18 12:50:17 -0700

    parser: setup unit testing

---
+++ cortecs/parser/test/tokenizer.c
@@ -0,0 +1,27 @@
+#include <unity.h>
+#include <tokenizer.h>
+#include <sds.h>
+#include <string.h>
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void test_tokenize_int(void) {
+    sds text = sdsnew("1");
+    cortecs_tokenizer_result_t result = cortecs_tokenizer_next(text, 0);
+    TEST_ASSERT_EQUAL_INT32(result.start, 1);
+    TEST_ASSERT_TRUE(result.token.tag == CORTECS_TOKEN_INT);
+    TEST_ASSERT_FALSE(strncmp(text, result.token.text, sdslen(text)));
+}
+
+// not needed when using generate_test_runner.rb
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_tokenize_int);
+    return UNITY_END();
+}
