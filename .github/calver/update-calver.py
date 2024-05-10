661e3baf6968019101a658c2c5a96cd97f86e06d
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-05-09 18:51:14 -0700

    ci: update version bump script to not submit noop PRs

--- .github/calver/update-calver.py
+++ .github/calver/update-calver.py
@@ -1,11 +1,14 @@
 import re
 from datetime import datetime
 from git import Repo
+import subprocess
+
 repo = Repo()
 head = repo.head.commit
 
 if "Updating module version" not in head.message:
     today = datetime.now().strftime('%Y.%m.%d')
+    today_hyphens = datetime.now().strftime('%Y-%m-%d')
 
     with open("MODULE.bazel", "r") as f:
         lines = f.readlines()
@@ -17,4 +20,12 @@ if "Updating module version" not in head.message:
                 f.write(line)
     index = repo.index
     index.add(["MODULE.bazel"])
-    index.commit("Updating module version to " + today)
+    message = "Updating module version to " + today
+    index.commit(message)
+    repo.remotes.origin.push(refspec="HEAD:version-bump/" + today_hyphens)
+    pr_link = subprocess.check_output(['gh', 'pr', 'create', '--title', message, '--body', 'Automatic PR for calver bump. Please ignore and have a nice day', '--base', 'main', '--head', 'version-bump/' + today_hyphens]).decode("utf-8")
+    for line in pr_link.splitlines():
+        if "https://github.com/reutermj/cortecs-c/pull/" in line:
+            pr_num = re.findall(r'\d+', line)[0]
+            subprocess.run(['gh', 'pr', 'comment', pr_num, '--body', '/trunk merge'])
+
