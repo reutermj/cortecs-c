d76dcff37b721687082ff0f9315ccd8410b29c59
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-05-22 08:30:17 -0700

    lexer: add testing for bad int tokens (#75)

--- cortecs/lexer/test/test.c
+++ cortecs/lexer/test/test.c
@@ -18,6 +18,166 @@ void tearDown() {
     // required for unity
 }
 
+cortecs_lexer_test_result_t lexer_test_bad_int_next(cortecs_lexer_test_state_t state, uint32_t entropy) {
+    switch (state.state) {
+        case 0: {
+            uint32_t next_state;
+            if (state.index == state.length - 2) {
+                // if this is the second to last char, transition to a state
+                // that can only generate bad suffixes
+                // matches [0-9][ac-km-rt-zAC-KM-RT-Z_]
+                next_state = 3;
+            } else {
+                // otherwise move to a state that can generate anything
+                next_state = 1;
+            }
+
+            return (cortecs_lexer_test_result_t){
+                .next_state = next_state,
+                .next_char = '0' + entropy % 10,
+            };
+        }
+        case 1: {
+            uint32_t i = entropy % 63;
+            char next_char;
+            uint32_t next_state;
+
+            if (i < 10) {
+                next_char = '0' + i;
+                if (state.index == state.length - 2) {
+                    // this digit is the second to last charater.
+                    // transition to a state that produces invalid unisigned suffixes
+                    // matches: [0-9]+[ac-km-rt-zAC-KM-RT-Z_]
+                    next_state = 3;
+                } else {
+                    // otherwise stay in this state
+                    next_state = 1;
+                }
+            } else if (i < 30) {
+                next_char = 'a' + (i - 10);
+                next_state = 2;
+            } else if (i == 30) {
+                next_char = 'u';
+                if (state.index == state.length - 2) {
+                    // 'u' is the second to last charater.
+                    // transition to a state that produces invalid unisigned suffixes
+                    // matches: [0-9]+u[ac-km-rt-zAC-KM-RT-Z_]
+                    next_state = 3;
+                } else {
+                    // otherwise the token is already bad, so transition to the
+                    // state that produces anything
+                    next_state = 2;
+                }
+            } else if (i < 36) {
+                next_char = 'v' + (i - 31);
+                next_state = 2;
+            } else if (i < 56) {
+                next_char = 'A' + (i - 36);
+                next_state = 2;
+            } else if (i == 56) {
+                next_char = 'U';
+                if (state.index == state.length - 2) {
+                    // 'u' is the second to last charater.
+                    // transition to a state that produces invalid unisigned suffixes
+                    // matches: [0-9]+U[ac-km-rt-zAC-KM-RT-Z_]
+                    next_state = 3;
+                } else {
+                    // otherwise the token is already bad, so transition to the
+                    // state that produces anything
+                    next_state = 2;
+                }
+            } else if (i < 62) {
+                next_char = 'V' + (i - 57);
+                next_state = 2;
+            } else {
+                next_char = '_';
+                next_state = 2;
+            }
+
+            return (cortecs_lexer_test_result_t){
+                .next_state = next_state,
+                .next_char = next_char,
+            };
+        }
+        case 2: {
+            // A bad int has already been generated. Generate anything
+            // matches: [0-9]+(([uU][a-zA-Z_])|([ac-km-rt-zAC-KM-RT-Z_]))[a-zA-Z0-9_]*
+            uint32_t i = entropy % 63;
+            char next_char;
+
+            if (i < 10) {
+                next_char = '0' + i;
+            } else if (i < 36) {
+                next_char = 'a' + (i - 10);
+            } else if (i < 62) {
+                next_char = 'A' + (i - 36);
+            } else {
+                next_char = '_';
+            }
+
+            return (cortecs_lexer_test_result_t){
+                .next_state = 2,
+                .next_char = next_char,
+            };
+        }
+        default: {
+            // only generates a bad suffix for the last char
+            // matches: [0-9]+[uU]?[ac-km-rt-zAC-KM-RT-Z_]
+            uint32_t i = entropy % 47;
+            char next_char;
+
+            if (i == 0) {
+                next_char = 'a';
+            } else if (i < 10) {
+                next_char = 'c' + (i - 1);
+            } else if (i < 16) {
+                next_char = 'm' + (i - 10);
+            } else if (i < 23) {
+                next_char = 't' + (i - 16);
+            } else if (i == 23) {
+                next_char = 'A';
+            } else if (i < 33) {
+                next_char = 'C' + (i - 24);
+            } else if (i < 39) {
+                next_char = 'M' + (i - 33);
+            } else if (i < 46) {
+                next_char = 'T' + (i - 39);
+            } else {
+                next_char = '_';
+            }
+
+            return (cortecs_lexer_test_result_t){
+                .next_char = next_char,
+            };
+        }
+    }
+}
+
+uint32_t lexer_test_bad_int_max_entropy(uint32_t state) {
+    switch (state) {
+        case 0:
+            return 10;
+        case 1:
+            return 63;
+        case 2:
+            return 63;
+        default:
+            return 47;
+    }
+}
+
+void cortecs_lexer_test_bad_int(void) {
+    cortecs_lexer_test_config_t stm = {
+        .next = &lexer_test_bad_int_next,
+        .should_skip_token = &cortecs_lexer_test_never_skip,
+        .state_max_entropy = &lexer_test_bad_int_max_entropy,
+        .tag = CORTECS_LEXER_TAG_BAD_INT,
+        .min_length = 2,
+    };
+    cortecs_lexer_test_fuzz(stm);
+    cortecs_lexer_test_exhaustive(stm);
+}
+
 cortecs_lexer_test_result_t lexer_test_int_next(cortecs_lexer_test_state_t state, uint32_t entropy) {
     switch (state.state) {
         case 0: {
@@ -592,6 +752,7 @@ int main() {
     RUN_TEST(lexer_test_space);
 
     RUN_TEST(cortecs_lexer_test_int);
+    RUN_TEST(cortecs_lexer_test_bad_int);
     RUN_TEST(cortecs_lexer_test_float);
     RUN_TEST(cortecs_lexer_test_bad_float);
 
