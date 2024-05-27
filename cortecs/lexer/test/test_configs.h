f6d17dff86e91de68bd8e722f5cf6d12a3848b39
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-05-27 15:13:02 -0700

    lexer: add new lines to multi token tests (#124)

--- cortecs/lexer/test/test_configs.h
+++ cortecs/lexer/test/test_configs.h
@@ -11,5 +11,6 @@ extern cortecs_lexer_test_config_t cortecs_lexer_test_bad_float_config;
 extern cortecs_lexer_test_config_t cortecs_lexer_test_int_config;
 extern cortecs_lexer_test_config_t cortecs_lexer_test_bad_int_config;
 extern cortecs_lexer_test_config_t cortecs_lexer_test_invalid_config;
+extern cortecs_lexer_test_config_t cortecs_lexer_test_new_line_config;
 
 #endif
