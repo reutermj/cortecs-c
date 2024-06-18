d9616c9ae3aa91e92e23d456739bda7611e9b940
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-06-18 07:05:06 -0700

    lsp: create LSP gen script
    Create initial script to generate LSP message structs from the LSP spec and run it on a few basic message types.

---
+++ source/lsp/cgen.py
@@ -0,0 +1,277 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import List
+import re
+
+@dataclass
+class BlockComment:
+    lines: List[str]
+
+@dataclass
+class TypeAnnotation:
+    name: str
+    cname: str
+    is_array: bool
+
+null_type = TypeAnnotation("null", "null", False)
+
+@dataclass 
+class FieldUnion:
+    comment: BlockComment
+    name: str
+    is_optional: bool
+    is_nulllable: bool
+    annotations: List[TypeAnnotation]
+
+@dataclass
+class FieldStruct:
+    comment: BlockComment
+    name: str
+    is_optional: bool
+    fields: Field
+
+@dataclass
+class FieldSingle:
+    comment: BlockComment
+    name: str
+    is_optional: bool
+    is_nulllable: bool
+    annotation: TypeAnnotation
+
+Field = FieldUnion | FieldStruct | FieldSingle
+
+@dataclass
+class Interface:
+    parent: str
+    name: str
+    fields: List[Field]
+
+with open("source/lsp/input.ts", "r") as f:
+    text = f.read()
+
+text = text.replace(":", " : ").replace(";", " ; ")
+tokens = text.split()
+
+tab = "    "
+
+token_stream = {
+    "tokens": tokens,
+    "index": 0
+}
+
+camel_case_re = re.compile('((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))')
+def to_snake_case(input):
+    return camel_case_re.sub(r'_\1', input.replace("?", "")).lower()
+
+def accept(pattern, token_stream):
+    index = token_stream["index"]
+    token = token_stream["tokens"][index]
+    if re.fullmatch(pattern, token):
+        token_stream["index"] = index + 1
+        return token
+    else:
+        return None
+
+def expect(pattern, token_stream):
+    token = accept(pattern, token_stream)
+    if token is not None:
+        return token
+    else:
+        index = token_stream["index"]
+        token = token_stream["tokens"][index]
+        message = "Expected token matching pattern " + pattern + " but found " + token
+        raise RuntimeError(message)
+
+def parse_block_comment(token_stream):
+    if accept("/[*]+", token_stream) is None:
+        return None
+    
+    lines = [""]
+    while accept("[*]+/", token_stream) is None:
+        if accept("[*]", token_stream) is not None:
+            lines[-1] = lines[-1].strip()
+            lines.append("")
+        else:
+            text = expect(".+", token_stream) + " "
+            lines[-1] = lines[-1] + text
+    lines[-1] = lines[-1].strip()
+    return BlockComment(lines)
+
+def parse_field_struct(comment, token_stream, name, is_optional):
+    fields = parse_fields(token_stream)
+    return FieldStruct(comment, name, is_optional, fields)
+
+def parse_fields(token_stream):
+    fields = []
+    while accept("}", token_stream) is None:
+        fields.append(parse_field(token_stream))
+    return fields
+
+def get_type(token_stream):
+    type_token = expect("[a-zA-Z]+(\[\])?", token_stream)
+    is_array = False
+    if "[]" in type_token:
+        type_token = type_token.replace("[]", "")
+        is_array = True
+
+    if type_token == "uinteger":
+        cname = "uint32_t"
+    elif type_token == "integer":
+        cname = "int32_t"
+    elif type_token == "string":
+        cname = "string_t"
+    elif type_token == "null":
+        cname = "null"
+    elif type_token == "object":
+        cname = "lsp_any"
+    elif type_token == "array":
+        cname = "lsp_array"
+    elif type_token.startswith("LSP"):
+        cname = to_snake_case(type_token)
+        type_token = cname.replace("lsp_", "")
+    elif type_token[0].isupper():
+        type_token = to_snake_case(type_token)
+        cname = "lsp_" + type_token
+    else:
+        raise RuntimeError("Unexpected type: " + type_token)
+    
+    return TypeAnnotation(type_token, cname, is_array)
+
+def parse_field_union(comment, token_stream, first_type, name, is_optional):
+    types = [first_type]
+    while True:
+        types.append(get_type(token_stream))
+        if accept("[|]", token_stream) is None:
+            break
+
+    
+    is_nullable = null_type in types
+    if is_nullable:
+        types.remove(null_type)
+
+    if len(types) == 1:
+        return FieldSingle(comment, name, is_optional, is_nullable, types[0])
+    else:
+        return FieldUnion(comment, name, is_optional, is_nullable, types)
+
+def parse_field(token_stream):
+    comment = parse_block_comment(token_stream)
+    name_full = expect("[a-zA-Z]+[?]?", token_stream)
+    is_optional = "?" in name_full
+    name = to_snake_case(name_full)
+    expect(":", token_stream)
+
+    if accept("{", token_stream) is not None:
+        field = parse_field_struct(comment, token_stream, name, is_optional)
+    else:
+        ctype = get_type(token_stream)
+        if accept("[|]", token_stream) is not None:
+            field = parse_field_union(comment, token_stream, ctype, name, is_optional)
+        else:
+            field = FieldSingle(comment, name, is_optional, False, ctype)
+
+    expect(";", token_stream)
+    return field
+
+def parse_interface(token_stream):
+    accept("extern", token_stream)
+    expect("interface", token_stream)
+    name = "lsp_" + to_snake_case(expect("[a-zA-Z]+", token_stream))
+
+    if accept("extends", token_stream) is not None:
+        parent = "lsp_" + to_snake_case(expect("[a-zA-Z]+", token_stream))
+    else:
+        parent = None
+    expect("{", token_stream)
+    fields = parse_fields(token_stream)
+    return Interface(parent, name, fields)
+
+def ctype_to_name(ctype):
+    if ctype == "char *":
+        return "string"
+    elif "*" in ctype:
+        return ctype.replace("*", "").strip()
+    else:
+        return ctype.strip()
+    
+def print_block_comment(comment, num_tabs):
+    if comment is None:
+        return
+    print((tab * num_tabs) + "/**", end='')
+    first = True
+    for line in comment.lines:
+        if first:
+            first = False
+        else:
+            print((tab * num_tabs) + " * ", end='')
+
+        print(line)
+    print((tab * num_tabs) + " */")
+
+def print_fields(fields, num_tabs, prefix):
+    first = True
+    for field in fields:
+        if first:
+            first = False
+        else:
+            print()
+
+        match field:
+            case FieldSingle(comment, name, is_optional, is_nullable, annotation):
+                print_block_comment(comment, num_tabs)
+                ctype = annotation.cname
+                if annotation.is_array:
+                    ctype = ctype + " *"
+                else:
+                    ctype = ctype + " "
+
+                if is_optional:
+                    print((tab * num_tabs) + "struct {")
+                    print((tab * (num_tabs + 1)) + "bool is_set;")
+                    if annotation.is_array:
+                        print((tab * (num_tabs + 1)) + "uint32_t length;")
+                    print((tab * (num_tabs + 1)) + ctype + "elements;")
+                    print((tab * num_tabs) + "} " + name + ";")
+                else:
+                    print((tab * num_tabs) + ctype + name + ";")
+            case FieldStruct(comment, name, is_optional, fields):
+                print_block_comment(comment, num_tabs)
+                print((tab * num_tabs) + "struct {")
+                if is_optional:
+                    print((tab * (num_tabs + 1)) + "bool is_set;")
+                print_fields(fields, num_tabs + 1, prefix + "_" + name)
+                print((tab * num_tabs) + "} " + name + ";")
+            case FieldUnion(comment, name, is_optional, is_nullable, annotations):
+                print_block_comment(comment, num_tabs)
+                print((tab * num_tabs) + "struct {")
+                if is_optional:
+                    print((tab * (num_tabs + 1)) + "bool is_set;")
+                print((tab * (num_tabs + 1)) + "enum {")
+                for annotation in annotations:
+                    print((tab * (num_tabs + 2)) + prefix.upper() + "_" + annotation.name.upper() + ",")
+                if is_nullable:
+                    print((tab * (num_tabs + 2)) + prefix.upper() + "_NULL,")
+                print((tab * (num_tabs + 1)) + "} tag;")
+                print((tab * (num_tabs + 1)) + "union {")
+                for annotation in annotations:
+                    ctype = annotation.cname
+                    if annotation.is_array:
+                        # TODO need to deal with arrays
+                        print((tab * (num_tabs + 2)) + "struct {")
+                        print((tab * (num_tabs + 3)) + "uint32_t length;")
+                        print((tab * (num_tabs + 3)) + ctype + " *elements;")
+                        print((tab * (num_tabs + 2)) + "} " + annotation.name + ";")
+                    else:
+                        ctype = ctype + " "
+                        print((tab * (num_tabs + 2)) + ctype + annotation.name + ";")
+                print((tab * (num_tabs + 1)) + "} value;")
+                print((tab * num_tabs) + "} " + name + ";")
+
+            
+
+interface = parse_interface(token_stream)
+print("typedef struct {")
+if interface.parent is not None:
+    print(tab + interface.parent + " super;")
+print_fields(interface.fields, 1, interface.name)
+print("} " + interface.name + ";")
---
+++ source/lsp/lsp.h
@@ -0,0 +1,72 @@
+#ifndef LSP_LSP_H
+#define LSP_LSP_H
+
+#include <persistent_string.h>
+#include <stdbool.h>
+
+typedef struct {
+    enum {
+        LSP_ANY_STRING,
+        LSP_ANY_INTEGER,
+        LSP_ANY_UINTEGER,
+        LSP_ANY_DECIMAL,
+        LSP_ANY_BOOLEAN,
+        LSP_ANY_NULL,
+    } tag;
+
+    union {
+        string_t string;
+        int32_t integer;
+        uint32_t uinteger;
+        float decimal;
+        bool boolean;
+    } value;
+} lsp_any;
+
+typedef struct {
+    uint32_t length;
+    lsp_any *content;
+} lsp_array;
+
+typedef struct {
+    string_t jsonrpc;
+} lsp_message;
+
+typedef struct {
+    lsp_message super;
+    /**
+     * The request id.
+     */
+    struct {
+        enum {
+            LSP_REQUEST_MESSAGE_INTEGER,
+            LSP_REQUEST_MESSAGE_STRING,
+        } tag;
+        union {
+            int32_t integer;
+            string_t string;
+        } value;
+    } id;
+
+    /**
+     * The method to be invoked.
+     */
+    string_t method;
+
+    /**
+     * The method's params.
+     */
+    struct {
+        bool is_set;
+        enum {
+            LSP_REQUEST_MESSAGE_ARRAY,
+            LSP_REQUEST_MESSAGE_OBJECT,
+        } tag;
+        union {
+            lsp_array array;
+            lsp_any object;
+        } value;
+    } params;
+} lsp_request_message;
+
+#endif
