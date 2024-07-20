b2d048c9ba2a4a0839a5d150af0af1308de1312c
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-07-20 11:01:43 -0700

    gc: new deferred reference count implementation
    new deferred reference counting GC. disabling GC tests until API stability. Also need to figure out if tracing and RC need to be split into two apis or if they can be unified.

--- source/cortecs/gc/rc/gc.c
+++ source/cortecs/gc/rc/gc.c
@@ -1,124 +1,122 @@
 #include <assert.h>
+#include <flecs.h>
 #include <gc.h>
 #include <world.h>
 
 // currently only allowing buffer size of 256. Will expand later
 #define CORTECS_GC_ALLOC_MAX_SIZE 256
 typedef struct {
+    uint32_t count;
+    ecs_entity_t entity;
     uint8_t data[CORTECS_GC_ALLOC_MAX_SIZE];
 } gc_buffer;
 static ECS_COMPONENT_DECLARE(gc_buffer);
 
-typedef struct {
-    uint32_t count;
-} reference_count;
-static ECS_COMPONENT_DECLARE(reference_count);
-
-static ecs_entity_t referenced_by;
-
 static void on_inc(ecs_iter_t *iterator) {
-    reference_count *count = ecs_field(iterator, reference_count, 1);
+    gc_buffer *count = ecs_field(iterator, gc_buffer, 0);
     for (int i = 0; i < iterator->count; i++) {
+        printf("incing\n");
         count->count++;
     }
 }
 
 static void on_dec(ecs_iter_t *iterator) {
-    reference_count *count = ecs_field(iterator, reference_count, 1);
+    gc_buffer *count = ecs_field(iterator, gc_buffer, 0);
+    int32_t target_index = ecs_query_find_var(iterator->query, "Target");
+
     for (int i = 0; i < iterator->count; i++) {
+        printf("decing\n");
         count->count--;
         if (count->count == 0) {
-            ecs_delete(world, iterator->entities[i]);
+            printf("deleting\n");
+            ecs_entity_t target = ecs_iter_get_var(iterator, target_index);
+            ecs_delete(world, target);
         }
     }
 }
 
-static void remove_unused(ecs_iter_t *iterator) {
-    for (int i = 0; i < iterator->count; i++) {
-        ecs_delete(world, iterator->entities[i]);
-    }
-}
+static ecs_entity_t inc;
+static ecs_entity_t dec;
 
 void cortecs_gc_init() {
-    referenced_by = ecs_new(world);
-    ECS_COMPONENT_DEFINE(world, reference_count);
     ECS_COMPONENT_DEFINE(world, gc_buffer);
     // marking the buffer as sparse makes sure it doesnt get moved and
     // pointers to it are stable
     ecs_add_id(world, ecs_id(gc_buffer), EcsSparse);
 
-    ecs_observer_desc_t inc_desc = {
+    inc = ecs_new(world);
+    ecs_entity_desc_t inc_reference_count_entity = {
+        .name = "inc_reference_count_system",
+        .add = ecs_ids(ecs_dependson(EcsPostFrame)),
+    };
+    ecs_system_desc_t inc_reference_count_system = {
+        .entity = ecs_entity_init(world, &inc_reference_count_entity),
         .query = {
             .terms[0] = {
-                .first.id = referenced_by,
-                .second.id = EcsWildcard,
+                .src.name = "$Target",
+                .first.id = ecs_id(gc_buffer),
             },
-            .terms[1].id = ecs_id(reference_count),
+            .terms[1] = {
+                .src.id = EcsThis,
+                .first.id = inc,
+                .second.name = "$Target",
+            },
         },
-        .events[0] = EcsOnAdd,
         .callback = on_inc,
     };
-    ecs_observer_init(world, &inc_desc);
+    ecs_system_init(world, &inc_reference_count_system);
 
-    ecs_observer_desc_t dec_desc = {
+    dec = ecs_new(world);
+    ecs_entity_desc_t dec_reference_count_entity = {
+        .name = "dec_reference_count_system",
+        .add = ecs_ids(ecs_dependson(EcsPostFrame)),
+    };
+    ecs_system_desc_t dec_reference_count_system = {
+        .entity = ecs_entity_init(world, &dec_reference_count_entity),
         .query = {
             .terms[0] = {
-                .first.id = referenced_by,
-                .second.id = EcsWildcard,
+                .src.name = "$Target",
+                .first.id = ecs_id(gc_buffer),
             },
-            .terms[1].id = ecs_id(reference_count),
+            .terms[1] = {
+                .src.id = EcsThis,
+                .first.id = dec,
+                .second.name = "$Target",
+            },
         },
-        .events[0] = EcsOnRemove,
         .callback = on_dec,
     };
-    ecs_observer_init(world, &dec_desc);
-
-    ecs_id_t run_on_post_frame[2] = {ecs_dependson(EcsPostFrame), 0};
-    ecs_entity_desc_t unused_memory_system = {
-        .name = "remove_unused_allocations",
-        .add = run_on_post_frame,
-    };
-
-    ecs_system_desc_t unused_desc = {
-        .entity = ecs_entity_init(world, &unused_memory_system),
-        .query = {
-            .terms[0].first.id = ecs_id(reference_count),
-            .terms[1] = {
-                .first.id = referenced_by,
-                .second.id = EcsWildcard,
-                .oper = EcsNot,
-            },
-        },
-        .callback = &remove_unused,
-    };
-    ecs_system_init(world, &unused_desc);
+    ecs_system_init(world, &dec_reference_count_system);
 }
 
-cortecs_gc_allocation_t cortecs_gc_alloc(uint32_t size) {
+void gc_dec(ecs_entity_t target) {
+    ecs_entity_t source = ecs_new(world);
+    ecs_add_pair(world, source, dec, target);
+}
+
+void *cortecs_gc_alloc(uint32_t size) {
     assert(size <= CORTECS_GC_ALLOC_MAX_SIZE);
 
     ecs_entity_t entity = ecs_new(world);
-    void *memory = ecs_emplace_id(world, entity, ecs_id(gc_buffer), NULL);
-    ecs_set(world, entity, reference_count, {.count = 0});
+    gc_buffer *memory = ecs_emplace_id(world, entity, ecs_id(gc_buffer), NULL);
+    memory->entity = entity;
+    memory->count = 1;
+    gc_dec(entity);
 
-    return (cortecs_gc_allocation_t){
-        .memory = memory,
-        .entity = entity,
-    };
+    return memory->data;
 }
 
-void cortecs_gc_add_root(ecs_entity_t target, cortecs_gc_allocation_t reference) {
-    ecs_add_pair(world, reference.entity, referenced_by, target);
+ecs_entity_t get_allocation_entity(void *allocation) {
+    gc_buffer *buffer = (gc_buffer *)(((uintptr_t)allocation) - offsetof(gc_buffer, data));
+    return buffer->entity;
 }
 
-void cortecs_gc_remove_root(ecs_entity_t target, cortecs_gc_allocation_t reference) {
-    ecs_remove_pair(world, reference.entity, referenced_by, target);
+void cortecs_gc_inc(void *allocation) {
+    ecs_entity_t target = get_allocation_entity(allocation);
+    ecs_entity_t source = ecs_new(world);
+    ecs_add_pair(world, source, inc, target);
 }
 
-void cortecs_gc_add(cortecs_gc_allocation_t target, cortecs_gc_allocation_t reference) {
-    cortecs_gc_add_root(target.entity, reference);
-}
-
-void cortecs_gc_remove(cortecs_gc_allocation_t target, cortecs_gc_allocation_t reference) {
-    cortecs_gc_remove_root(target.entity, reference);
+void cortecs_gc_dec(void *allocation) {
+    gc_dec(get_allocation_entity(allocation));
 }
