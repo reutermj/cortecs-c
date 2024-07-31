09dbb923566120159e0a13a15d7a56c3829ebe97
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-07-31 09:06:03 -0500

    gc: add initial tests for rc gc (#252)

--- source/cortecs/gc/gc.h
+++ source/cortecs/gc/gc.h
@@ -1,11 +1,13 @@
 #ifndef CORTECS_GC_GC_H
 #define CORTECS_GC_GC_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 void *cortecs_gc_alloc(uint32_t size);
 void cortecs_gc_init();
 void cortecs_gc_inc(void *allocation);
 void cortecs_gc_dec(void *allocation);
+bool cortecs_gc_is_alive(void *allocation);
 
 #endif
