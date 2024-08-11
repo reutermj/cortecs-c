f044545bb4ac6c9af2861abffc8d9f482132c906
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-08-11 08:43:40 -0700

    common: Update common macros and switch to C11
    Couple small changes and bugfixes to the common macros. As well, switch to c11 for atomics

--- source/common/common.h
+++ source/common/common.h
@@ -10,9 +10,10 @@
     int CONCAT(NOOP_, __LINE__) = 0; \
     UNUSED(CONCAT(NOOP_, __LINE__));
 
-#define MASK(BITS) (~(UINT64_MAX << BITS))
+#define MASK(BITS, SHIFT) (~(UINT64_MAX << BITS) << SHIFT)
+#define MAX_VALUE(BITS) (~(UINT64_MAX << BITS))
 
 #define COMPILE_TIME_ASSERT(predicate) \
-    static char CONCAT(compile_time_assertion_, line)[!!(predicate)-1];
+    static char CONCAT(compile_time_assertion_, __LINE__)[!!(predicate)-1];
 
 #endif
