0d33f381e1066bc1ce9fe21e2b2803392978474d
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-08-14 15:43:54 -0700

    gc: add array allocation

--- source/cortecs/gc/gc.h
+++ source/cortecs/gc/gc.h
@@ -8,6 +8,7 @@ typedef void (*cortecs_gc_finalizer)(void *allocation);
 typedef uint16_t cortecs_gc_type_index;
 
 void *cortecs_gc_alloc(uint32_t size, cortecs_gc_type_index type_index);
+void *cortecs_gc_alloc_array(uint32_t size, uint32_t elements, cortecs_gc_type_index type);
 cortecs_gc_type_index cortecs_gc_register_type(cortecs_gc_finalizer finalizer, uint32_t size);
 void cortecs_gc_init();
 void cortecs_gc_inc(void *allocation);
