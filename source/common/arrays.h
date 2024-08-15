2130c490696e95c668ee0908df05743377756e8b
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-08-15 08:07:23 -0700

    arrays: create standard format for arrays

---
+++ source/common/arrays.h
@@ -0,0 +1,24 @@
+#ifndef COMMON_ARRAYS_H
+#define COMMON_ARRAYS_H
+
+#include <common.h>
+#include <stdint.h>
+
+#define array_declare(TYPE) \
+    typedef struct {        \
+        uint32_t size;      \
+        TYPE elements[];    \
+    } CONCAT(array_, TYPE);
+
+#define array(TYPE) CONCAT(array_, TYPE)*
+
+array_declare(int8_t);
+array_declare(int16_t);
+array_declare(int32_t);
+array_declare(int64_t);
+array_declare(uint8_t);
+array_declare(uint16_t);
+array_declare(uint32_t);
+array_declare(uint64_t);
+
+#endif
