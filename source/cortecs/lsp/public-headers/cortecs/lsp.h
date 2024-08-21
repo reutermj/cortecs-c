82a2f6f4f0c90c8a1e474b31fe8d2ffdbb3b7163
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-08-21 09:57:15 -0500

    lsp: update namespacing for lsp files (#304)

--- source/cortecs/lsp/public-headers/cortecs/lsp.h
+++ source/cortecs/lsp/public-headers/cortecs/lsp.h
@@ -2,37 +2,38 @@
 #define CORTECS_LSP_LSP_H
 
 #include <cortecs/array.h>
+#include <cortecs/gc.h>
 #include <cortecs/string.h>
 #include <stdbool.h>
 
 typedef struct {
     enum {
-        LSP_PARSE_SUCCESS,
-        LSP_PARSE_SUCCESS_NOT_FOUND,
-        LSP_PARSE_MISSING_REQUIRED_FIELD,
-        LSP_PARSE_TYPE_ERROR,
+        CORTECS_LSP_PARSE_SUCCESS,
+        CORTECS_LSP_PARSE_SUCCESS_NOT_FOUND,
+        CORTECS_LSP_PARSE_MISSING_REQUIRED_FIELD,
+        CORTECS_LSP_PARSE_TYPE_ERROR,
     } tag;
     cortecs_string message;
-} lsp_parse_error_t;
+} cortecs_lsp_parse_error_t;
 
-typedef struct lsp_any lsp_any;
-cortecs_array_forward_declare(lsp_any);
+typedef struct cortecs_lsp_any cortecs_lsp_any;
+cortecs_array_forward_declare(cortecs_lsp_any);
 
 typedef struct {
     cortecs_array(cortecs_string) field_names;
-    cortecs_array(lsp_any) field_values;
-} lsp_object;
+    cortecs_array(cortecs_lsp_any) field_values;
+} cortecs_lsp_object;
 
-struct lsp_any {
+struct cortecs_lsp_any {
     enum {
-        LSP_ANY_STRING,
-        LSP_ANY_INTEGER,
-        LSP_ANY_UINTEGER,
-        LSP_ANY_DECIMAL,
-        LSP_ANY_BOOLEAN,
-        LSP_ANY_OBJECT,
-        LSP_ANY_ARRAY,
-        LSP_ANY_NULL,
+        CORTECS_LSP_ANY_STRING,
+        CORTECS_LSP_ANY_INTEGER,
+        CORTECS_LSP_ANY_UINTEGER,
+        CORTECS_LSP_ANY_DECIMAL,
+        CORTECS_LSP_ANY_BOOLEAN,
+        CORTECS_LSP_ANY_OBJECT,
+        CORTECS_LSP_ANY_ARRAY,
+        CORTECS_LSP_ANY_NULL,
     } tag;
 
     union {
@@ -41,18 +42,18 @@ struct lsp_any {
         uint32_t uinteger;
         float decimal;
         bool boolean;
-        lsp_object object;
-        cortecs_array(lsp_any) array;
+        cortecs_lsp_object object;
+        cortecs_array(cortecs_lsp_any) array;
     } value;
 };
-cortecs_array_define(lsp_any);
+cortecs_array_define(cortecs_lsp_any);
 
 typedef struct {
     cortecs_string jsonrpc;
-} lsp_message;
+} cortecs_lsp_message;
 
 typedef struct {
-    lsp_message super;
+    cortecs_lsp_message super;
     /**
      * The method to be invoked.
      */
@@ -64,14 +65,14 @@ typedef struct {
     struct {
         bool is_set;
         enum {
-            LSP_NOTIFICATION_MESSAGE_PARAMS_ARRAY,
-            LSP_NOTIFICATION_MESSAGE_PARAMS_OBJECT,
+            CORTECS_LSP_NOTIFICATION_MESSAGE_PARAMS_ARRAY,
+            CORTECS_LSP_NOTIFICATION_MESSAGE_PARAMS_OBJECT,
         } tag;
         union {
-            cortecs_array(lsp_any) array;
-            lsp_object object;
+            cortecs_array(cortecs_lsp_any) array;
+            cortecs_lsp_object object;
         } value;
     } params;
-} lsp_notification_message;
+} cortecs_lsp_notification_message;
 
 #endif
