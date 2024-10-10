ac7d9e27aa78277f08a1fe973a306f9527189083
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-10-10 07:01:27 -0700

    ci: update ci commands
    Dont run clang-tidy on templates because they're intentionally not valid c files. Fix future issue in how the find commands were run overflowing the terminal line max size

--- .github/workflows/local-ci.sh
+++ .github/workflows/local-ci.sh
@@ -3,11 +3,18 @@
 bazel clean
 rm compile_commands.json
 
-clang-format --dry-run --Werror $(find source/ -iname '*.h' -o -iname '*.c')
-clang-format --dry-run --Werror $(find test/ -iname '*.h' -o -iname '*.c')
+# verify file formatting
+find source/ -iname '*.h' -o -iname '*.c' -exec clang-format --dry-run --Werror {} \;
+find test/ -iname '*.h' -o -iname '*.c' -exec clang-format --dry-run --Werror {} \;
+
+# build compile_commands.json
 CC=clang bazel build //...
 CC=clang bazel run @hedron_compile_commands//:refresh_all
-clang-tidy --config-file ./source/.clang-tidy -p ./ $(find source/. -name "*.c")
-clang-tidy --config-file ./test/.clang-tidy -p ./ $(find test/. -name "*.c")
+
+# run linter
+find source/ -type f -iname '*.c' ! -iname '*.template.c' -exec clang-tidy --config-file ./source/.clang-tidy -p ./ {} \;
+find test/ -type f -iname '*.c' -exec clang-tidy --config-file ./test/.clang-tidy -p ./ {} \;
+
+# run code coverate
 CC=gcc bazel coverage --instrumentation_filter //source/... --combined_report=lcov //test/...
 genhtml --branch-coverage --output genhtml "$(bazel info output_path)/_coverage/_coverage_report.dat"
--- source/cortecs/string/string.c
+++ source/cortecs/string/string.c
@@ -33,28 +33,29 @@ cortecs_string cortecs_string_new(const char *format, ...) {
     va_list args_out;
     va_start(args_size, format);
     va_copy(args_out, args_size);
+    cortecs_string ret = NULL;
 
     // measure the output string
     // there seems to be a bug in clang tidy that's false positive on this line
     int32_t size = vsnprintf(NULL, 0, format, args_size);  // NOLINT(clang-analyzer-valist.Uninitialized)
     if (size < 0) {
         // TODO encoding error. Have better error reporting
-        return NULL;
+        goto cleanup;
     }
 
     // write the output string
-    cortecs_string out = cortecs_gc_alloc_impl(
+    ret = cortecs_gc_alloc_impl(
         offsetof(cortecs_string_impl, content) + size + 1,
         CORTECS_FINALIZER_NONE,
         __FILE__,
         __func__,
         __LINE__
     );
-    out->size = size;
-    vsnprintf(out->content, size + 1, format, args_out);
+    ret->size = size;
+    vsnprintf(ret->content, size + 1, format, args_out);
 
+cleanup:
     va_end(args_size);
     va_end(args_out);
-
-    return out;
+    return ret;
 }
