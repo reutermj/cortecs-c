3015e8f98e2682013745f9490dd81448083340db
Mark Reuter <13319190+reutermj@users.noreply.github.com>
2024-11-04 06:47:19 -0800

    Correct namespacing of define number types

--- source/cortecs/types/public-headers/cortecs/types.h
+++ source/cortecs/types/public-headers/cortecs/types.h
@@ -1,49 +1,50 @@
 #ifndef CORTECS_TYPES_TYPES_H
 #define CORTECS_TYPES_TYPES_H
 
+#include <cortecs/mangle.h>
 #include <stdint.h>
 
-typedef uint8_t U8;
-typedef uint16_t U16;
-typedef uint32_t U32;
-typedef uint64_t U64;
+typedef uint8_t CN(Cortecs, U8);
+typedef uint16_t CN(Cortecs, U16);
+typedef uint32_t CN(Cortecs, U32);
+typedef uint64_t CN(Cortecs, U64);
 
-typedef int8_t I8;
-typedef int16_t I16;
-typedef int32_t I32;
-typedef int64_t I64;
+typedef int8_t CN(Cortecs, I8);
+typedef int16_t CN(Cortecs, I16);
+typedef int32_t CN(Cortecs, I32);
+typedef int64_t CN(Cortecs, I64);
 
-typedef float F32;
-typedef double F64;
+typedef float CN(Cortecs, F32);
+typedef double CN(Cortecs, F64);
 
-#define TYPE_PARAM_T U8
+#define TYPE_PARAM_T CN(Cortecs, U8)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T U16
+#define TYPE_PARAM_T CN(Cortecs, U16)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T U32
+#define TYPE_PARAM_T CN(Cortecs, U32)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T U64
+#define TYPE_PARAM_T CN(Cortecs, U64)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T I8
+#define TYPE_PARAM_T CN(Cortecs, I8)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T I16
+#define TYPE_PARAM_T CN(Cortecs, I16)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T I32
+#define TYPE_PARAM_T CN(Cortecs, I32)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T I64
+#define TYPE_PARAM_T CN(Cortecs, I64)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T F32
+#define TYPE_PARAM_T CN(Cortecs, F32)
 #include "ptr.template.h"
 
-#define TYPE_PARAM_T F64
+#define TYPE_PARAM_T CN(Cortecs, F64)
 #include "ptr.template.h"
 
 #endif
